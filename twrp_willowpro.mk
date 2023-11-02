7caae8379717cfeac3bdbf30f79973ed009c1904
nguyenphuthai2k6 <136307896+nguyenphuthai2k6@users.noreply.github.com>
2023-11-03 06:05:57 +0700

    twrp_willowpro.mk

--- twrp_willowpro.mk
+++ twrp_willowpro.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$device/vsmart/willowpro_o
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := willowpro_open
-PRODUCT_DEVICE := twrp_willowpro
+PRODUCT_DEVICE := willowpro
 PRODUCT_MODEL := Star 4
 PRODUCT_BRAND := vsmart
 PRODUCT_MANUFACTURER := vsmart
