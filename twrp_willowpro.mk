afd0ae9d68f2e2c82a455552268274980eb44146
nguyenphuthai2k6 <nguyenphuthai2k6@gmail.com>
2023-11-04 17:27:44 +0700

    twrp_willowpro.mk

--- twrp_willowpro.mk
+++ twrp_willowpro.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-DEVICE_PATH := device/vsmart/willowpro_open
+DEVICE_PATH := device/vsmart/willowpro
 
 # Release name
 PRODUCT_RELEASE_NAME := vsmart Star 4
